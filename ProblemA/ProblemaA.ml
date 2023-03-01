f59d3fef002b28a887489840f6bcc2d381095717
SniperPRX <tigo.lopes.ribeiro@gmail.com>
2023-03-01 19:31:49 +0000

    Update ProblemaA.ml

--- ProblemA/ProblemaA.ml
+++ ProblemA/ProblemaA.ml
@@ -1,23 +1,30 @@
+open Z;;
+
+(*funções de input que dada uma linha, divide-a pelo espaço e retorna dois inteiros a e b*)
+  let line = read_line ();;
+  let line_parts = String.split_on_char ' ' line;; 
+  let a = int_of_string (List.nth line_parts 0);; 
+  let b = int_of_string (List.nth line_parts 1);;
+
+(*função recursiva com a função auxiliar somatorio*)
+(*função recursiva do somatório da equação*)
+let count= ref 0;;
 let rec f1 n=
+  count := !count+1;
+  let rec soma n =
+    let rec soma_aux k n acc =
+      if k > n-2 then acc 
+      else soma_aux (k+1) n (acc+(f1(k)*f1(n-k-1))) in 
+    soma_aux 1 n 0 in
   if n=0 then 1 
   else if n=1 then 2
-  else 
-    let rec soma k = if k > n - 2 then 0 else k + soma (f1(k) * f1(n-k-1)) in
-     (3 * f1 (n-1)) + soma (f1(k) * f1(n-k-1))
+  else 3* (f1 (n-1)) + (soma (n))
 
 
-
-
-
-
-
-
-
-
+let count2 = ref 0;;
 
 let rec f2 n=
-  if n=0 then 1 , 1
-  else if n=1 then 2 , 1 
-  else 
-    let (a , b) , (c , d) = f2 (n-1) , f2 (n-2) in 
-    ((((6 * n - 3) * a) - ((n - 2) * c)) / (n + 1) , b + d + 1)
+  count2 := !count2 + 1;
+  if n = 0 then 1
+  else if n=1 then 2
+  else ((((6*n-3)*f2 (n-1)) - ((n-2)*f2 (n-2)))/(n+1))
