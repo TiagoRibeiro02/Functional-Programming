93fe5be3483f36edf4afc66cc651ff384dc54561
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-04-22 22:02:18 +0100

    Update ProblemaB.ml

--- ProblemB/ProblemaB.ml
+++ ProblemB/ProblemaB.ml
@@ -1,8 +1,8 @@
   let main=
-  let n = Scanf.scanf "%d" (fun n -> n) (* leitura do tamanho do bolo *)
-  let m = Scanf.scanf "%d" (fun m -> m) (* leitura do número de tamanhos de fatias *)
+  let n = read_line()in (* leitura do tamanho do bolo *)
+  let m = read_line()in (* leitura do número de tamanhos de fatias *)
   (* leitura dos tamanhos e preços das fatias *)
-  let fatias = Array.make m (0, 0)
+  let fatias = Array.make m (0, 0)in
   for i = 0 to m-1 do
     let tamanho, preco = Scanf.sscanf (read_line()) "%d %d" (fun x y -> (x, y)) in
     fatias.(i) <- (tamanho, preco)
