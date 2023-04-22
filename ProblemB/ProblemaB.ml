22b619cd8ad8bb173cbb464e3c56953b219905c7
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-04-22 19:18:05 +0100

    Update ProblemaB.ml

--- ProblemB/ProblemaB.ml
+++ ProblemB/ProblemaB.ml
@@ -1,24 +1,9 @@
-let cut_cake n prices =
-  let dp = Array.make_matrix (n+1) (n+1) 0 in
-  for i = 1 to n do
-    for j = 1 to n do
-      if j < i then
-        dp.(i).(j) <- dp.(i-1).(j)
-      else
-        dp.(i).(j) <- max dp.(i-1).(j) (dp.(i).(j-i) + prices.(i-1))
-    done;
-  done;
-  let rec find_cuts i j acc =
-    if i <= 0 || j <= 0 then
-      acc
-    else if dp.(i).(j) = dp.(i-1).(j) then
-      find_cuts (i-1) j acc
-    else
-      find_cuts i (j-i) (i :: acc)
-  in
-  let cuts = find_cuts n n [] in
-  dp.(n).(n), cuts
-
   let main=
   let n = read_int() (* leitura do tamanho do bolo *)
   let m = read_int() (* leitura do número de tamanhos de fatias *)
+  (* leitura dos tamanhos e preços das fatias *)
+  let fatias = Array.make m (0, 0)
+  for i = 0 to m-1 do
+    let tamanho, preco = Scanf.sscanf (read_line()) "%d %d" (fun x y -> (x, y)) in
+    fatias.(i) <- (tamanho, preco)
+  done
