e9e3d844dc86a1f80c62c92ae42768ac6fb6172b
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-04-24 15:48:17 +0100

    .

--- ProblemB/ProblemaB.ml
+++ ProblemB/ProblemaB.ml
@@ -8,13 +8,14 @@
 
 
 let n = read_int();; (* tamanho do bolo *)
-let m = read_int();; (* numero de fatias *)
+let m = read_int();; (* numero de tamanhos de fatias *)
 
 (* tamanho e preço das fatias *)
 let precos = Array.make m (0, 0);;
-for i = 0 to m-1 do
-  let tamanho, preco = Scanf.sscanf (read_line()) "%d %d" (fun x y -> (x, y)) in
-  precos.(i) <- (tamanho, preco)
+if n,m<0 || n,m>10000 || m > n then failwith "Error" else
+for x = 0 to m-1 do
+  let tamanho, preco = Scanf.sscanf (read_line()) "%d %d" (fun i j -> (i, j)) in
+  precos.(x) <- (tamanho, preco)
 done;;
 
 (* vetor dp -> lucro máximo *)
@@ -31,4 +32,4 @@ if n <= Array.length dp - 1 then
   let lucro_maximo = dp.(n) in
   Printf.printf "%d\n" lucro_maximo
 else
-  Printf.printf "Erro: n fora dos limites do vetor dp\n"
+  Printf.printf "Erro\n"
--- ProblemB/teste.ml
+++
@@ -1,7 +0,0 @@
-let m = read_int() in
-
-let precos = Array.make m (0, 0) in
-for i = 0 to m-1 do
-  let tamanho, preco = Scanf.sscanf (read_line()) "%d %d" (fun x y -> (x, y)) in
-  precos.(i) <- (tamanho, preco)
-done
